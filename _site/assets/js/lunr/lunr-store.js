91884f789dc8f16b8cf3c005af3bf6036906a746
baird92487 <langenbrunner@gmail.com>
2018-07-09 19:58:39 -0700

    draft post

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,6 +1,6 @@
 var store = [{
-        "title": "Basic plots with matplotlib",
-        "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["matplotlib"],
+        "title": "Simple plots with matplotlib",
+        "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["coding"],
         "tags": [],
-        "url": "http://localhost:4000/matplotlib/2016/08/05/simple-matplotlib-plots.html",
+        "url": "http://localhost:4000/coding/2018/07/09/simple-matplotlib-plots.html",
         "teaser":null}]
