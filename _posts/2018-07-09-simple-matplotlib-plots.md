e3c0388e3aa0fc77cf43d9e156f75fa93fa5895a
baird92487 <langenbrunner@gmail.com>
2018-07-09 19:55:37 -0700

    draft post

--- _posts/2018-07-09-simple-matplotlib-plots.md
+++ _posts/2018-07-09-simple-matplotlib-plots.md
@@ -1,8 +1,8 @@
 ---
 layout: category-post
-title:  "Welcome to Jekyll!"
+title:  'Basic plots with matplotlib'
 date:   2016-08-05 20:20:56 -0400
-categories: writing
+categories: matplotlib
 ---
 You’ll find this post in your `_posts` directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run `jekyll serve`, which launches a web server and auto-regenerates your site when a file is updated.
 
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,6 +1,6 @@
 var store = [{
-        "title": "Welcome to Jekyll!",
-        "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["writing"],
+        "title": "Basic plots with matplotlib",
+        "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["matplotlib"],
         "tags": [],
-        "url": "http://localhost:4000/writing/2016/08/05/simple-matplotlib-plots.html",
+        "url": "http://localhost:4000/matplotlib/2016/08/05/simple-matplotlib-plots.html",
         "teaser":null}]
