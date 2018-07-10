8c04c7027d6c99c1093ed8cf937c201eb00b14c7
baird92487 <langenbrunner@gmail.com>
2018-07-09 19:46:53 -0700

    draft post

--- _pages/resources.md
+++ _pages/resources.md
@@ -7,7 +7,7 @@ order: 2
 
 Some of my favorite online resources are listed below.
 
-**Learning how to code in Pytho**
+**Learning how to code in Python**
 * If you're totally new to coding, I like [Think Python](https://greenteapress.com/wp/think-python-2e/) by Allen B. Downey
   * See also his [Think Stats](http://greenteapress.com/wp/think-stats-2e/) and [Think Bayes](http://greenteapress.com/wp/think-bayes/) books for statistics
 
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,6 @@
-var store = []
+var store = [{
+        "title": "Welcome to Jekyll!",
+        "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["writing"],
+        "tags": [],
+        "url": "http://localhost:4000/writing/2016/08/05/simple-matplotlib-plots.html",
+        "teaser":null}]
