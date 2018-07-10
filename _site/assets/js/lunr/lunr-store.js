85acbea27795f27df5e355f57abc520c62e5d7e1
baird92487 <langenbrunner@gmail.com>
2018-07-09 20:07:12 -0700

    draft post

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,6 +1,6 @@
 var store = [{
         "title": "Simple plots with matplotlib",
-        "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["coding"],
+        "excerpt":"Matplotlib saving best practices: bbox_inches = tight transparent=True dpi=300 or dpi=600 for .pngYou’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run...","categories": ["coding"],
         "tags": [],
         "url": "http://localhost:4000/coding/2018/07/09/simple-matplotlib-plots.html",
         "teaser":null}]
